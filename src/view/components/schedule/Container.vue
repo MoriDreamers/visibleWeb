948850781339dae5441b47e10acc499ca2f04498
MoriDreamers <louhaoyu@outlook.com>
2025-05-07 14:36:03 +0800

    deployment_container_基础 && 端口配置功能

--- src/view/components/schedule/Container.vue
+++ src/view/components/schedule/Container.vue
@@ -5,6 +5,9 @@ import { storeToRefs } from 'pinia';
 import { onBeforeMount } from 'vue';
 import { toRefs } from 'vue';
 import { ElMessage } from 'element-plus';
+import StringToList from './../StringToList.vue'
+import Port from './container/Port.vue'
+
 
 //拿到pinia的全局状态管理 
 const useItemer = useItem()
@@ -12,8 +15,9 @@ const { item } = storeToRefs(useItem());
 const props = defineProps({
     containerType: {
         type: String,
-        default: 'contianer'
+        default: 'container'
         //调用初始化模块使用参数init
+
     }
 })
 
@@ -22,14 +26,30 @@ const data = reactive({
     containers:[],
     container:{
         name: `${props.containerType}${containerNum.value}`,
-        image: 'nginx:latest',
+        image: '',
+        tty: false,
+        workingDir: '',
+        // 镜像下载策略
+        imagePullPolicy:"IfNotPresent",
+        resources:{
+            limits:{},
+            requests:{},
+        },
+        ports:[],
+    },
+    options:{
+        imagePullPolicyList:[
+            { value: 'IfNotPresent', label: '未找到就拉取' },
+            { value: 'Always', label: '总是' },
+            { value: 'Never', label: '从不' },
+        ]
     }
 })
 
 const editableTabsValue = ref(0);
 const removeTab = (targetName) => {
     console.log(targetName)
-    if (data.containers.length === 1 && props.containerType === 'contianer') {
+    if (data.containers.length === 1 && props.containerType === 'container') {
         ElMessage({
     showClose: true,
     message: '请至少保留一个容器！！',
@@ -47,7 +67,7 @@ const removeTab = (targetName) => {
 
 
 onBeforeMount(() => {
-    if (props.containerType !== 'contianer'){
+    if (props.containerType !== 'container'){
         data.containers = useItemer.item.spec.template.spec.initContainers
     }else{
         data.containers = useItemer.item.spec.template.spec.containers
@@ -69,9 +89,13 @@ const addTab = () => {
     // 这里设置了默认焦点在data的containers的最后一个索引
     editableTabsValue.value = data.containers.length - 1
 }
-const {containers,initContainers} = toRefs(data)
+const {containers, options} = toRefs(data)
 
 const activeName = ref('basic');
+
+const seleteChanged = (value) => {
+    console.log('镜像下载策略已更改为:', value)
+}
 
 </script>
 
@@ -92,18 +116,108 @@ const activeName = ref('basic');
         >
         <el-tab-pane
             v-for="(item, index) in containers"
-            :key="item.name"
+            :key="index"
             :label="item.name"
             :name="index"
             >
 
 
-            <el-tabs v-model="activeName" @tab-click="handleClick" tab-position="left">
+            <el-tabs v-model="activeName" tab-position="left">
                 <el-tab-pane label="基本配置" name="basic">
-                    
+                  <div>
+                    <el-row :gutter="20" justify="space-between">
+                        <el-col :span="8">
+                            <!-- 因为实际上rlus检测的是item表单这个，所以要使用required 就需要这样写
+                            prop="('spec.template.spec.containers.'+index+'.name')"
+                            -->
+                            <el-form-item label="容器名称" prop="item.name" >
+                                <el-input v-model.trim="item.name" placeholder="请输入容器名称"></el-input>
+                            </el-form-item>
+                        </el-col>
+
+                        <el-col :span="8">
+                            <el-form-item label="是否绑定终端（创建一个终端让容器不会自动关闭）" prop="item.tty" >
+                                <el-switch v-model="item.tty" active-color="#13ce66" inactive-color="#ff4949" active-text="启用" inactive-text="禁用"></el-switch>
+                            </el-form-item>
+                        </el-col>
+                        
+                        <el-col :span="8">
+                            <el-form-item label="自定义工作目录" prop="item.workingDir" >
+                                <el-input v-model.trim="item.workingDir"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                    <el-row :gutter="20" justify="space-between">
+                        <el-col :span="16">
+                            <el-form-item label="镜像地址" prop="item.image" >
+                                <el-input v-model.trim="item.image" placeholder="请输入镜像地址"></el-input>
+                            </el-form-item>
+                        </el-col>
+
+                        <el-col :span="8">
+                            <el-form-item label="镜像下载策略" prop="item.imagePullPolicy" >
+                                <el-select
+                                    v-model="item.imagePullPolicy"
+                                    placeholder="选择镜像下载"
+                                    @change="seleteChanged"
+                                >
+                                    <el-option
+                                    v-for="s in options.imagePullPolicyList"
+                                    :key="s.value"
+                                    :label="s.label"
+                                    :value="s.value"
+                                    />
+                                </el-select>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                    <el-row :gutter="20" justify="space-between">
+                        <el-col :span="12">
+                            <el-form-item label="启动命令" prop="item.command">
+                                <StringToList v-model:data="item.command" placeholder="请输入启动命令，以回车分隔（EnteyPoint）"></StringToList>
+                            </el-form-item>
+                        </el-col>
+
+                        <el-col :span="12">
+                            <el-form-item label="启动参数" prop="item.args" >
+                                <StringToList v-model:data="item.args" placeholder="请输入启动参数，以回车分隔（CMD）"></StringToList>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                    <el-row :gutter="20" justify="space-between">
+                        <el-col :span="6">
+                            <el-form-item label="最小内存" prop="item.resources.requests.memory">
+                                <el-input v-model.trim="item.resources.requests.memory" placeholder="格式：1024m"></el-input>
+                            </el-form-item>
+                        </el-col>
+
+                        <el-col :span="6">
+                            <el-form-item label="最大内存" prop="item.resources.limits.memory" >
+                                <el-input v-model.trim="item.resources.limits.memory" placeholder="格式：2048m"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        
+                        <el-col :span="6">
+                            <el-form-item label="最小CPU" prop="item.resources.requests.cpu" >
+                                <el-input v-model.trim="item.resources.requests.cpu" placeholder="格式：100m"></el-input>
+                            </el-form-item>
+                        </el-col>
+
+                        <el-col :span="6">
+                            <el-form-item label="最大CPU" prop="item.resourse.limits.cpu" >
+                                <el-input v-model.trim="item.resources.limits.cpu" placeholder="格式：1000m (1核)"></el-input>
+                            </el-form-item>
+                        </el-col>
+
+                    </el-row>
+
+                    </div>
                 </el-tab-pane>
                 <el-tab-pane label="端口配置" name="ports">
-                    Config
+                    <Port :ports="item.ports"> </Port>
                     </el-tab-pane>
                 <el-tab-pane label="健康检查" name="healthcheck">
                     Role
---
+++ src/view/components/schedule/container/Port.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    ports: {
+        type: Array,
+        default: () => []
+    }
+})
+
+
+const addTab = () => {
+    const newPort=({
+        name: '',
+        containerPort: 80,
+        hostPort: 0,
+        protocol: 'TCP'
+    })
+    props.ports.unshift(newPort)
+}
+
+const deleteTab = (index) => {
+    props.ports.splice(index, 1)
+}
+</script>
+
+<template>
+    <el-table
+    :data="props.ports"
+    align="center"
+    >
+        <el-table-column label="端口名称" >
+            <template #default="scope">
+                <el-input v-model.trim="scope.row.name" placeholder="请输入端口名称"></el-input>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="容器端口" >
+            <template #default="scope">
+                <el-input-number 
+                    v-model="scope.row.containerPort" 
+                    :min="1" 
+                    :max="65535" 
+                    :controls="false"
+                    placeholder="请输入容器端口">
+                </el-input-number>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="主机端口" >
+            <template #default="scope">
+                <el-input-number 
+                    v-model="scope.row.hostPort" 
+                    :min="1" 
+                    :max="65535" 
+                    :controls="false"
+                    placeholder="请输入主机端口">
+                </el-input-number>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="协议" >
+            <template #default="scope">
+                <el-select v-model="scope.row.protocol" placeholder="请选择协议">
+                    <el-option label="TCP" value="TCP"></el-option>
+                    <el-option label="UDP" value="UDP"></el-option>
+                    <el-option label="SCTP" value="SCTP"></el-option>
+                </el-select>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="操作" width="120">
+            <template #header>
+                <el-button link type="primary" @click="addTab">添加</el-button>
+            </template>
+            <template #default="scope">
+                <el-button type="danger" link @click="deleteTab(scope.$index)">删除</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
+
+<style scoped>
+.el-input-number {
+    width: 100%;
+}
+:deep(.el-input-number .el-input__wrapper) {
+    padding-left: 8px;
+}
+</style>
+
